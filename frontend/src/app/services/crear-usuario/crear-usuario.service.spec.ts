9e381e697ff81f2b78761c736aee0a938ae6c411
Henry Galvez <soulofhenry@gmail.com>
2020-09-14 17:46:31 -0600

    prueba para la funcion post hacia la api (exito)

--- frontend/src/app/services/crear-usuario/crear-usuario.service.spec.ts
+++ frontend/src/app/services/crear-usuario/crear-usuario.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CrearUsuarioService } from './crear-usuario.service';
 
 fdescribe('CrearUsuarioService', () => {
   let service: CrearUsuarioService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        ]
+    });
     service = TestBed.inject(CrearUsuarioService);
   });
 
@@ -16,13 +20,12 @@ fdescribe('CrearUsuarioService', () => {
 
   it('deberia existir la importacion de la url de la api', () => {
     let base64 = service.getbaseURL()
-    console.log(base64)
     expect(base64).toBeTruthy();
   });
 
   it('deberia existir la funcion post a la api', () => {
-    let base64 = service.post()
-    expect(base64).toBeTruthy();
+    let res = service.postUser
+    expect(res).toBeTruthy();
   });
 
 });
--- frontend/src/app/services/crear-usuario/crear-usuario.service.ts
+++ frontend/src/app/services/crear-usuario/crear-usuario.service.ts
@@ -12,4 +12,8 @@ export class CrearUsuarioService {
   getbaseURL(): any{
     return baseURL
   }
+
+  postUser(user: any): any{
+    return true
+  }
 }
