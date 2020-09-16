5ff1dd02854e8a2fd4d0f4f499d53fea6c15f6e8
Edward Gomez <2741424220101@ingenieria.usac.edu.gt>
2020-09-15 20:23:45 -0600

    Se agrego prueba que verifica endpoint que obtiene tareas

---
+++ frontend/src/app/services/controlNotasEstudiante/control-notas-estudiante.service.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ControlNotasEstudianteService } from './control-notas-estudiante.service';
+
+fdescribe('ControlNotasEstudianteService', () => {
+  let service: ControlNotasEstudianteService;
+  let data = {
+    id_usuario: 1,
+    id_clase: 1
+  }
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        ]
+    });
+    service = TestBed.inject(ControlNotasEstudianteService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('verificacion de funcionamiento del endpoint que obtiene tareas', () => {
+    expect(service.get_tareas(data)).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/services/controlNotasEstudiante/control-notas-estudiante.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseURL';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ControlNotasEstudianteService {
+
+  constructor(private  http:HttpClient) { }
+
+}
